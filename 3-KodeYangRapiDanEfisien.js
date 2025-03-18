a209a7d42b1bbbd4be5cedcad1e1a524115eca68
udep1001 <koalasipemalas@gmail.com>
2025-03-18 23:53:56 +0800

    Create 3-KodeYangRapiDanEfisien.js

---
+++ 3-KodeYangRapiDanEfisien.js
@@ -0,0 +1,10 @@
+//Contoh buruk (acak-acakan dan ga jelas):
+function a(x){return x*2}b=a(5);console.log(b);
+
+//Contoh baik (rapi dan jelas):
+function kalikanDua(angka) {
+    return angka * 2;
+}
+
+let hasil = kalikanDua(5);
+console.log(hasil);
